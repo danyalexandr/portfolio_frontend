07902755f5b2a224eba86cc7768826f7fd31d725
danyalexandr <danyalexandr@gmail.com>
2023-01-01 18:48:08 -0300

    experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HardAndSoftSkillsComponent,
+    ProyectsComponent
   ],
   imports: [
     BrowserModule,
